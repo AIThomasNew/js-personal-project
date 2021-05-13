fd086a38b963b856ba3183bfd00dd3ea16449333
Thomas <thomas.new.it@gmail.com>
2021-05-13 23:13:00 +0500

    первая попытка создания кошерного модального окна для регистрации

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-// let userName = prompt('Как вас зовут?', '');
-
-// function ask(userName) {
-//   if (userName == true) {
-//     return alert('Приветствую тебя ' + userName + '!');
-//   } else {
-//     return alert('Ладно, познакомимся позже');
-//   }
-// }
-
-// ask(userName);
+const $ = {
+  
+};
