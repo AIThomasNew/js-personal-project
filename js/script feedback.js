94d26dc4ccba4950adde3b0eb2005d61033a37b3
Thomas <thomas.new.it@gmail.com>
2021-06-03 18:17:40 +0500

    add change scss anim

--- js/script feedback.js
+++ js/script feedback.js
@@ -1,16 +1,14 @@
-var swiper = new Swiper('.mySwiper', {
+let swiper = new Swiper('.mySwiper', {
   effect: 'coverflow',
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: 'auto',
   coverflowEffect: {
-    rotate: 50,
+    rotate: 0,
     stretch: 0,
     depth: 100,
-    modifier: 1,
+    modifier: 2,
     slideShadows: true,
   },
-  pagination: {
-    el: '.swiper-pagination',
-  },
+  loop: true,
 });
