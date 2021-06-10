f91a0772a8b9c5d767d1c18bb0703f542d21995e
Thomas <thomas.new.it@gmail.com>
2021-06-10 22:04:44 +0500

    весь день создавал модальное окно для оформления заказа

--- plugins/modal.js
+++ plugins/modal.js
@@ -3,24 +3,50 @@ function _createModal(options) {
   modal.classList.add('modal-block');
   modal.insertAdjacentHTML(
     'afterbegin',
-    `<div class="modal-wrap">
-  <div class="modal-window">
-    <div class="modal-header">
-      <span class="modal-title">Модальное окно</span>
-      <span class="modal-close">&times</span>
+    `<section class="modal-wrap">
+    <div class="modal-window">
+      <div class="contactInfo">
+        <div>
+          <h2></h2>
+        </div>
+      </div>
+      <div class="modal-form">
+        <div class="modal-header">
+          <span class="modal-close"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+          </svg></span>
+        </div>
+        <div class="contactForm">
+          <h2>Оформить заказ</h2>
+          <div class="formBox">
+            <div class="inputBox w50">
+              <input type="text" required />
+              <span>Имя</span>
+            </div>
+            <div class="inputBox w50">
+              <input type="text" required />
+              <span>Фамилия</span>
+            </div>
+            <div class="inputBox w50">
+              <input type="email" required />
+              <span>Email</span>
+            </div>
+            <div class="inputBox w50">
+              <input type="text" required />
+              <span>Номер телефона</span>
+            </div>
+            <div class="inputBox w100">
+              <textarea></textarea>
+              <span>Ваш комментарий...</span>
+            </div>
+            <div class="inputBox w100">
+              <input id="submit-order" type="submit" value="Заказать" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="modal-body">
-      <p>Электронная почта</p>
-      <p><input type="text"></p>
-      <p>Пароль</p>
-      <p><input type="text"> <input type="checkbox"></p>
-    </div>
-    <div class="modal-footer">
-      <button>Oк</button>
-      <button>Отмена</button>
-    </div>
-  </div>
-</div>`
+  </section>`
   );
   document.body.appendChild(modal);
   return modal;
