4cc6b14df025f79e25f353abf2fe30f683940c55
Thomas <thomas.new.it@gmail.com>
2021-06-18 20:33:47 +0500

    крутятся

--- js/eye-anim.js
+++ js/eye-anim.js
@@ -1,4 +1,16 @@
+// Относительно документа
+
+
+
 function eyeball() {
   const eyeAnim = document.querySelectorAll('.eyeAnim');
-  eyeAnim.forEach
+  eyeAnim.forEach(function (eyeAnim) {
+    let x = eyeAnim.getBoundingClientRect().left + eyeAnim.clientWidth / 2;
+    let y = eyeAnim.getBoundingClientRect().top + eyeAnim.clientHeight / 2;
+
+    let radian = Math.atan2(event.pageX - x, event.pageY - y);
+    let rotation = radian * (180 / Math.PI) * -1 + 270;
+
+    eyeAnim.style.transform = 'rotate(' + rotation + 'deg)';
+  });
 }
