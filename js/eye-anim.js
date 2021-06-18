c9a8d643743a889acf8d6fe9ee2c506db0e0972a
Thomas <thomas.new.it@gmail.com>
2021-06-18 20:49:51 +0500

    глаза следят за курсором только когда проводишь снизу err.

--- js/eye-anim.js
+++ js/eye-anim.js
@@ -1,12 +1,14 @@
+// при движении мыши в body
+
+document.querySelector('body').addEventListener('mousemove', eyeball);
+
 // Относительно документа
-
-
 
 function eyeball() {
   const eyeAnim = document.querySelectorAll('.eyeAnim');
   eyeAnim.forEach(function (eyeAnim) {
-    let x = eyeAnim.getBoundingClientRect().left + eyeAnim.clientWidth / 2;
-    let y = eyeAnim.getBoundingClientRect().top + eyeAnim.clientHeight / 2;
+    let x = (eyeAnim.getBoundingClientRect().left) + (eyeAnim.clientWidth / 2);
+    let y = (eyeAnim.getBoundingClientRect().top) + (eyeAnim.clientHeight / 2);
 
     let radian = Math.atan2(event.pageX - x, event.pageY - y);
     let rotation = radian * (180 / Math.PI) * -1 + 270;
